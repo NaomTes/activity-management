9b6ca16c69ef7306bd3b670d2bbf713c9bea2a7c
Awais Ahmed <awaishere007@gmai.com>
2020-08-24 12:51:14 +0500

    Schema is generated for service model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_075651) do
+ActiveRecord::Schema.define(version: 2020_08_21_105816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 2020_08_21_075651) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_providers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_providers_on_reset_password_token", unique: true
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
   end
 
 end
