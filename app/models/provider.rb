d2f9fb4c05387b407b6a005b239df37430eb203a
Awais Ahmed <awaishere007@gmai.com>
2020-08-25 11:06:41 +0500

    Serializers are added for json response

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,35 @@
 class Provider < ApplicationRecord
+  include AuthHelper
   devise :database_authenticatable, :registerable, :validatable
 
   has_many :provided_services
   has_many :services, through: :provided_services
   has_many :availability_durations
+  validates :first_name,
+            presence: true,
+            length: { minimum: 5, maximum: 50 }
+  validates :last_name,
+            presence: true,
+            length: { minimum: 5, maximum: 50 }
+  validates :age,
+            presence: true,
+            numericality: { only_integer: true },
+            length: { minimum: 1, maximum: 3 }
+  validates :profile_description,
+            presence: true,
+            length: { minimum: 5, maximum: 100 }
+  validates :base_price,
+            presence: true,
+            numericality: true
+  # pattern validations are required
+  validates :street,
+            presence: true
+  validates :city,
+            presence: true
+  validates :postal_code,
+            presence: true,
+            numericality: true
+  validates :country,
+            presence: true
+  # end
 end
---
+++ app/serializers/provider_serializer.rb
@@ -0,0 +1,14 @@
+class ProviderSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :first_name,
+             :last_name,
+             :email,
+             :age,
+             :profile_description,
+             :base_price,
+             :street,
+             :city,
+             :postal_code,
+             :country,
+             :auth_token
+end
