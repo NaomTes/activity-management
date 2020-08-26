d85367e106282823a8b01718250d9c387128d4f1
Awais Ahmed <awaishere007@gmai.com>
2020-08-26 22:59:48 +0500

    Display name is added against provider model for active admin filter Name validation is changed

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -16,7 +16,7 @@ class Provider < ApplicationRecord
             :postal_code,
             :country, presence: true
 
-  validates :first_name, :last_name, length: { minimum: 5, maximum: 50 }
+  validates :first_name, :last_name, length: { minimum: 3, maximum: 50 }
 
   validates :age,
             numericality: { only_integer: true },
@@ -29,5 +29,9 @@ class Provider < ApplicationRecord
     provider = Provider.find_by(email: email)
     return nil unless provider.present?
     provider.valid_password?(password) ? provider : nil
+  end
+
+  def display_name
+    self.email
   end
 end
