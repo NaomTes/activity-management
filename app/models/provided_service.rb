38c03a2fe13b0e1b79f89ae3a8b29156c751e530
Awais Ahmed <awaishere007@gmai.com>
2020-08-26 12:28:49 +0500

    Delegate is added

--- app/models/provided_service.rb
+++ app/models/provided_service.rb
@@ -4,16 +4,14 @@ class ProvidedService < ApplicationRecord
   has_many :requested_services
   has_many :booked_services
 
-  def service_name
-    service.name
-  end
-
   validates :service_id, :uniqueness => { :scope => :provider_id }
 
   validates :service_description,
             :price, presence: true
 
   validates :price, numericality: true
+
+  delegate :name, to: :service
 
   validates :service_description, length: { minimum: 5, maximum: 50 }
 end
--- app/serializers/provider/provided_service_serializer.rb
+++ app/serializers/provider/provided_service_serializer.rb
@@ -3,7 +3,7 @@ class Provider::ProvidedServiceSerializer
   attributes :service_description,
              :price,
              :provider_id
-  attribute :service do |provided_service|
-    provided_service.service_name
+  attribute :service_name do |provided_service|
+    provided_service.name
   end
 end
