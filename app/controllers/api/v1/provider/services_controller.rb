0b2b529d94c23b0ccda26c32a9b74f8f218c0fe8
Awais Ahmed <awaishere007@gmai.com>
2020-08-28 23:56:58 +0500

    Service authorization is removed for its index page

--- app/controllers/api/v1/provider/services_controller.rb
+++ app/controllers/api/v1/provider/services_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::Provider::ServicesController < Api::V1::Provider::ApiController
-  before_action :authenticate
-
   def index
     @services = Service.all
     render json: Provider::ServiceSerializer.new(@services).serialized_json, status: :ok
