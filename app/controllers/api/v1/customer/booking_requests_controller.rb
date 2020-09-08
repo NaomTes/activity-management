b98713d05ca47090965a4c2158567d39280f2bd4
Awais Ahmed <awaishere007@gmai.com>
2020-09-08 23:34:19 +0500

    Atleast one requested service constraint is added for booking request

--- app/controllers/api/v1/customer/booking_requests_controller.rb
+++ app/controllers/api/v1/customer/booking_requests_controller.rb
@@ -2,12 +2,16 @@ class Api::V1::Customer::BookingRequestsController < Api::V1::Customer::ApiContr
   before_action :authenticate
 
   def create
-    @booking_request = current_customer.booking_requests.new(booking_request_params)
-    if @booking_request.save
-      render json: Customer::BookingRequestSerializer.new(@booking_request).serialized_json,
-             status: :created
+    if params[:booking_request][:requested_services_attributes].length == 0
+      render json: { errors: ["There must be atleast one requested service"] }, status: :not_found
     else
-      unprocessable_entity @booking_request
+      @booking_request = current_customer.booking_requests.new(booking_request_params)
+      if @booking_request.save
+        render json: Customer::BookingRequestSerializer.new(@booking_request).serialized_json,
+               status: :created
+      else
+        unprocessable_entity @booking_request
+      end
     end
   end
 
