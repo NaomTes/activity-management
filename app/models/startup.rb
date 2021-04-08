eb3537c8b62183af5a4d4596e1b8a3eb0730fb1c
Zohaib Butt <zohaibnadeem13@gmail.com>
2021-04-09 01:05:29 +0500

    add validation in startup

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,6 +1,7 @@
 class Startup < ActiveRecord::Base
+  validates :company_name, uniqueness: true
   
   def self.save_current_record(record_params)
-    create_with(record_params).find_or_create_by(email: record_params[:email])
+    create_with(record_params).find_or_create_by(company_name: record_params[:company_name])
   end
 end
