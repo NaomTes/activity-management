7a84a75862419ae967f97e91d9520954acf848c1
Zohaib Butt <zohaibnadeem13@gmail.com>
2021-08-07 12:31:28 +0500

    mail host changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { :host => 'ec2-3-143-225-62.us-east-2.compute.amazonaws.com' }
+  config.action_mailer.default_url_options = { :host => 'ec2-18-118-99-200.us-east-2.compute.amazonaws.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
